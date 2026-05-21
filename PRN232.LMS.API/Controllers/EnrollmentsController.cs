18eec9bf4cec0625251ad9355aec075bee18ec58
@fsa_ThinhTT <thinhttse182004@fpt.edu.vn>
2026-05-21 15:07:21 +0700

    test

--- PRN232.LMS.API/Controllers/EnrollmentsController.cs
+++ PRN232.LMS.API/Controllers/EnrollmentsController.cs
@@ -67,6 +67,7 @@ public class EnrollmentsController : ControllerBase
     /// <summary>Update an existing enrollment</summary>
     [HttpPut("{id:int}")]
     [ProducesResponseType(typeof(ApiResponse<EnrollmentResponse>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status404NotFound)]
     public async Task<IActionResult> Update(int id, [FromBody] UpdateEnrollmentRequest request)
     {
         if (!ModelState.IsValid)
@@ -81,6 +82,7 @@ public class EnrollmentsController : ControllerBase
     /// <summary>Delete an enrollment</summary>
     [HttpDelete("{id:int}")]
     [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status404NotFound)]
     public async Task<IActionResult> Delete(int id)
     {
         var deleted = await _service.DeleteAsync(id);
